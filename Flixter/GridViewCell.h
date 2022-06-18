8ad5d862e47f09185fb500f71186c90a1f9d92cd
Christina Li <cyli@fb.com>
2022-06-17 21:36:34 -0700

    added collection view, though buggy

--- Flixter/GridViewCell.h
+++ Flixter/GridViewCell.h
@@ -10,7 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GridViewCell : UICollectionViewCell
-@property (weak, nonatomic) IBOutlet UIImageView *movieImage;
+@property (weak, nonatomic) IBOutlet UIImageView *posterImage;
 @end
 
 NS_ASSUME_NONNULL_END
--- Flixter/GridViewController.m
+++ Flixter/GridViewController.m
@@ -6,8 +6,14 @@
 //
 
 #import "GridViewController.h"
+#import "GridViewCell.h"
 
-@interface GridViewController ()
+@interface GridViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray *movieArray;
+//@property (nonatomic, strong) UIRefreshControl *refreshControl;
+//@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -15,7 +21,71 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=d1df56c0ce66b8c88c4d0cf53ca88ea0"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    __weak typeof(self) weakSelf = self;
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+//            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies"
+//                                           message:@"The internet connection appears to be offline."
+//                                           preferredStyle:UIAlertControllerStyleAlert];
+//
+//            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+//               handler:^(UIAlertAction * action) {}];
+//
+//            [alert addAction:defaultAction];
+//            [self presentViewController:alert animated:YES completion:nil];
+        }
+        else {
+            typeof(self) strongSelf = weakSelf;
+            if (!strongSelf) {
+                return;
+            }
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+//            [self.activityIndicator stopAnimating];
+            
+            NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
+            
+            // TODO: Get the array of movies
+            // TODO: Store the movies in a property to use elsewhere
+            strongSelf.movieArray = dataDictionary[@"results"];
+            strongSelf.collectionView.dataSource = strongSelf;
+            strongSelf.collectionView.delegate = strongSelf;
+            
+            [strongSelf.collectionView reloadData];
+            
+//            [strongSelf.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier]; // order?
+//            strongSelf.tableView.rowHeight = 200;
+        
+            
+        }
+//        [self.refreshControl endRefreshing];
+    }];
+    [task resume];
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movieArray.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    GridViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"com.codepath.GridViewCell" forIndexPath:indexPath];
+    
+    NSDictionary *movie = self.movieArray[indexPath.row];
+
+    NSString *posterURLString = [@"https://image.tmdb.org/t/p/w500" stringByAppendingString:movie[@"poster_path"]];
+    cell.posterImage.image = nil;
+    NSData * imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: posterURLString]];
+    cell.posterImage.image = [UIImage imageWithData: imageData];
+    
+    return cell;
 }
 
 /*
